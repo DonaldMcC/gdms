059a38319666a0db489ef10418e171feec9a43cb
DonaldMcC <donaldm2020@gmail.com>
2015-10-23 22:40:54 +0100

    updated presentation and documentation links

--- controllers/plugin_ndspresent_slides.py
+++ controllers/plugin_ndspresent_slides.py
@@ -19,11 +19,18 @@ def split():
 
 def show():
     presentation = db(db.plugin_presentation.name == request.args[0]).select().first()
-    slides = db(db.plugin_slide.presentation_id == presentation.id).select()
+    if presentation:
+        slides = db(db.plugin_slide.presentation_id == presentation.id).select()
     
-    response.title = presentation.title
-    response.description = presentation.description
-    response.keywords = presentation.keywords
-    response.author = presentation.author
+        response.title = presentation.title
+        response.description = presentation.description
+        response.keywords = presentation.keywords
+        response.author = presentation.author
+    else:
+        response.title = 'Presentation not loaded'
+        response.description = 'Presentation not loaded'
+        response.keywords = 'Presentation not loaded'
+        response.author = 'Presentation not loaded'
+        slides = None
 
     return dict(slides=slides)
