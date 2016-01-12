ba815d550d90c4adaf618e1569629cd8f0a661bf
Russ King <newglobalstrategy@gmail.com>
2016-01-12 22:30:15 +0000

    Add initial search test

---
+++ fts/test_vol9_searchtest.py
@@ -0,0 +1,38 @@
+# These tests are all based on the tutorial at http://killer-web-development.com/
+# if registration is successful this may work but lets
+# try and get user logged in first
+
+
+from functional_tests import FunctionalTest, ROOT, USERS
+from ddt import ddt, data, unpack
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+@ddt
+class ClearQuests (FunctionalTest):
+
+    def setUp(self):
+        self.url = ROOT + '/default/user/login'        
+        get_browser = self.browser.get(self.url)
+
+        mailstring = USERS['USER1']+'@user.com'
+        email = self.browser.find_element_by_name("email")
+
+        email = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_name("email"))
+        email.send_keys(mailstring)
+
+        password = self.browser.find_element_by_name("password")    
+        password.send_keys(USERS['PASSWORD1'])
+
+        submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
+        submit_button.click()
+        time.sleep(1)    
+
+    def test_search(self):
+        self.url = ROOT + '/search/newindex.html'
+        get_browser = self.browser.get(self.url)
+        time.sleep(2)  # still getting blank category for some reason but not if loaded manually
+        # questiontext = self.browser.find_element_by_name('questiontext')
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Search Results', body.text)
