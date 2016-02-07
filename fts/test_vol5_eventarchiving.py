27a300845c5eb9ffde4f445335b5e13e56edb14d
DonaldM <donaldm2020@gmail.com>
2016-02-07 23:39:15 +0000

    test of event archiving

--- fts/test_vol5_eventarchiving.py
+++ fts/test_vol5_eventarchiving.py
@@ -9,85 +9,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-@ddt
-class AnswerQuestion (FunctionalTest):
+class TestEventPage(FunctionalTest):
 
-#    def setUp(self):
-#        self.url = ROOT + '/default/user/login'
-#        get_browser = self.browser.get(self.url)
-
-    @data((USERS['USER3'], USERS['PASSWORD3'], '2', 'not yet agreed'),
-          (USERS['USER2'], USERS['PASSWORD2'], '2', 'not yet agreed'),
-          (USERS['USER4'], USERS['PASSWORD4'], '2', 'is Agreed'))
-    @unpack
-    def test_answer(self, user, passwd, answer, result):
-        self.url = ROOT + '/default/user/login'
-        get_browser = self.browser.get(self.url)
-        time.sleep(1)
-        mailstring = user + '@user.com'
+    def testarchving(self):
+        self.url = ROOT + '/default/user/login'        
+        get_browser=self.browser.get(self.url)
+        mailstring = USERS['USER1'] + '@user.com'
         email = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_name("email"))
         email.send_keys(mailstring)
 
-        password = self.browser.find_element_by_name("password")
-        password.send_keys(passwd)
-        time.sleep(1)
+        password = self.browser.find_element_by_name("password")    
+        password.send_keys(USERS['PASSWORD1'])    
+  
         submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
         time.sleep(1)
-        submit_button.click()
+        submit_button.click()  
+        time.sleep(1)  
+        self.url = ROOT + '/'        
+        get_browser=self.browser.get(self.url)
         time.sleep(1)
-        self.url = ROOT + '/answer/get_question/action'
-        get_browser = self.browser.get(self.url)
-        time.sleep(1)
-        ansstring = "(//input[@name='ans'])[" + answer + "]"
-        for x in range(0, NUMCYCLES):
-            # self.browser.find_element_by_xpath("(//input[@name='ans'])[2]").click()
-            # toclick = WebDriverWait(self, 10).until(lambda self:
-            # self.browser.find_element_by_xpath("(//input[@name='ans'])[2]"))
-            wait = WebDriverWait(self.browser, 12)
-            element = wait.until(EC.element_to_be_clickable((By.XPATH, ansstring)))
-            element.click()
-            urgency = self.browser.find_element_by_id("userquestion_urgency")
-            urgency.send_keys("9")
-            importance = self.browser.find_element_by_id("userquestion_importance")
-            importance.send_keys("10")
-            self.browser.find_element_by_id("userquestion_changecat").click()
-
-            # category.select_by_visible_text("Strategy")
-            self.browser.find_element_by_id("userquestion_changescope").click()
-
-            activescope = self.browser.find_element_by_id("userquestion_activescope")
-            activescope.send_keys("2 Continental")
-
-            # self.browser.find_element_by_id("userquestion_answerreason").clear()
-            self.browser.find_element_by_id("userquestion_answerreason").send_keys("ph5 user2")
-
-            time.sleep(2)
-
-            category = self.browser.find_element_by_id("userquestion_category")
-            category.send_keys("Strategy")
-
-            continent = self.browser.find_element_by_id("userquestion_continent")
-            continent.send_keys("Europe (EU)")
-            # self.browser.find_element_by_id("userquestion_continent").select_by_visible_text("Africa (AF)")
-
-            # self.browser.find_element_by_id("userquestion_answerreason").clear()
-            # self.browser.find_element_by_id("userquestion_answerreason").send_keys(
-            # "the right answer selenium testing")
-            # driver.find_element_by_css_selector("input.btn").click()
-
-            # answer.send_keys("1")
-
-            submit_button = self.browser.find_element_by_css_selector("#submit_record__row input")
-            submit_button.click()
-            time.sleep(1)
-
-            body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
-            # self.assertIn('This question is in progress', body.text)
-            self.assertIn(result, body.text)
-
-            self.browser.find_element_by_xpath("//input[@value='Next Action']").click()
-            time.sleep(1)
-
+        driver.find_element_by_link_text("Healthcare Review").click()
+        reviewlink1 = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_link_text("Healthcare Review")) 
+        revewlink1.click()
+        reviewlink2 = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_xpath("//input[@value='Review Event']")) 
+        revewlink2.click()
+        reviewlink3 = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_xpath("//input[@value='Archive']")) 
+        revewlink3.click()
+        reviewlink4 = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_xpath("(//button[@type='button'])[4]")) 
+        revewlink4.click()
+        reviewlink5 = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_xpath("//input[@value='View Event']")) 
+        revewlink5.click()
+        time.sleep(3)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Archiving and Shared', body.text)
+        
         self.url = ROOT + '/default/user/logout'
-        get_browser = self.browser.get(self.url)
-        time.sleep(2)
+        get_browser=self.browser.get(self.url)
+        time.sleep(1)
+        
+
+
+
