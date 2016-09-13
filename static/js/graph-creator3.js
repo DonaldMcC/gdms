8b8f901f404fd45b81838e3d58473361c969b665
DonaldM <donaldm2020@gmail.com>
2016-09-13 22:33:21 +0100

    switch to toggle touchstart

--- static/js/graph-creator3.js
+++ static/js/graph-creator3.js
@@ -697,6 +697,8 @@ document.onload = (function(d3, saveAs, Blob, undefined){
       })
       .on("touchend", function(d){
         state.mouseDownLink = null;
+        document.getElementById('target').innerHTML = "";               
+        state.touchlinking = false;
       });
 
     // remove old links
@@ -721,7 +723,7 @@ document.onload = (function(d3, saveAs, Blob, undefined){
           
         d3.select(this).classed(consts.connectClass, false);
       })
-      .on("mousedown", function(d){
+      .on("mousedown", function(d){         
         thisGraph.circleMouseDown.call(thisGraph, d3.select(this), d);
       })
         .on("touchstart", function(d){
@@ -730,10 +732,11 @@ document.onload = (function(d3, saveAs, Blob, undefined){
                     //some sort of highlight of item and message to be generated
                     state.touchlinking = false; 
                     state.mouseDownNode = false;}
-                else {    
+                else {  
+                    document.getElementById('target').innerHTML = "Linking from " + d3.select(this).text();               
                     thisGraph.circleMouseDown.call(thisGraph, d3.select(this), d);
                     state.touchlinking = true;
-                    d.style("fill", "purple")};
+                    };
       })
       .on("mouseup", function(d){
         thisGraph.circleMouseUp.call(thisGraph, d3.select(this), d);
