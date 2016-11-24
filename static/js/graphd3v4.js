c29deafaefb59768e22ac10dd6c876b5cb7fb452
DonaldMcC <donaldm2020@gmail.com>
2016-11-24 22:09:09 +0000

    now got non-editable text in the boxes

--- static/js/graphd3v4.js
+++ static/js/graphd3v4.js
@@ -7,6 +7,24 @@
 
     // from donald method
 
+    var consts =  {
+    selectedClass: "selected",
+    connectClass: "connect-node",
+    circleGClass: "conceptG",
+    graphClass: "graph",
+    activeEditId: "active-editing",
+    BACKSPACE_KEY: 8,
+    DELETE_KEY: 46,
+    ENTER_KEY: 13,
+    nodeRadius: 80
+
+  };
+
+    var textHeight = 10;
+    var lineHeight = textHeight + 5;
+    var lines = [];
+   initLines()
+
         d3edges.forEach(function(e, i){
               d3edges[i] = {source: d3nodes.filter(function(n){return n.serverid == e.source;})[0],
                           target: d3nodes.filter(function(n){return n.serverid == e.target;})[0],
@@ -106,17 +124,24 @@ simulation.force("link")
 
     // add the nodes
     node.append('circle') /* 'circlej */
-        .attr('r', 20)
+        .attr('r', consts.nodeRadius)
         .style("fill", function(d){return d.fillclr})
         .style("stroke-width", function(d){return d.swidth})
         /* .attr('height', 25) */
         ;
 
-    // add text
+    /* add text
     node.append("text")
+            .attr("class", "node")
             .attr("x", 14)
             .attr("dy", "-1.2em")
-            .text(function(d) {return d.name;});
+            .wrapText(gEl, title, txtclr);*/
+
+
+    node.each(function(d) {
+        console.log(d.title);
+    wrapText(d3.select(this), d.title, d.txtclr)});
+
 
     node.on("mouseover", function(d) {
 
@@ -180,3 +205,92 @@ simulation.force("link")
             d.fx = null;
             d.fy = null;
         }
+
+
+
+ function wrapText(gEl, title) {
+
+     var i = 0;
+     var line = 0;
+     var words = title.split(" ");
+
+     var el = gEl.append("text")
+     //.style("fill", txtclr) not getting this to work and possibly not a good idea anyway
+         .attr("text-anchor", "middle")
+         .attr("font-size", "11px")
+         .attr("dy", "-" + 8 * 7.5);
+
+
+     while (i < lines.length && words.length > 0) {
+
+         line = lines[i++];
+         var lineData = calcAllowableWords(line.maxLength, words);
+         var tspan = el.append('tspan').text(lineData.text);
+         console.log(lineData.text);
+         if (i > 1)
+             tspan.attr('x', 0).attr('dy', '15');
+         words.splice(0, lineData.count);
+     }
+     ;
+ };
+
+        // calculate how many words will fit on a line
+
+function calcAllowableWords(maxWidth, words) {
+
+    var wordCount = 0;
+    var testLine = "";
+    var spacer = "";
+    var fittedWidth = 0;
+    var fittedText = "";
+    //ctx.font = font;
+
+    for (var i = 0; i < words.length; i++) {
+
+        testLine += spacer + words[i];
+        spacer = " ";
+
+        //var width = ctx.measureText(testLine).width;
+        var width = testLine.length * 5
+        if (width > maxWidth) {
+            return ({
+                count: i,
+                width: fittedWidth,
+                text: fittedText
+            });
+        }
+
+        fittedWidth = width;
+        fittedText = testLine;
+
+    }
+    return ({
+                count: i,
+                width: fittedWidth,
+                text: fittedText
+            });
+}
+
+function initLines() {
+
+  var radius  = 80;
+
+    for (var y = radius * .9; y > -radius; y -= lineHeight) {
+
+        var h = Math.abs(radius - y);
+
+        if (y - lineHeight < 0) {
+            h += 20;
+        }
+
+        var length = 2 * Math.sqrt(h * (2 * radius - h)) + 5;
+
+        if (length && length > 10) {
+            lines.push({
+                y: y,
+                maxLength: length
+            });
+        }
+
+    }
+}
