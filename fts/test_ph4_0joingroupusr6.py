cafd454ac23d317893027170131042a4c6ab641c
DonaldMcC <donaldm2020@gmail.com>
2017-01-19 23:12:13 +0000

    pep8 changes for fts phase 4

--- fts/test_ph4_0joingroupusr6.py
+++ fts/test_ph4_0joingroupusr6.py
@@ -5,15 +5,15 @@
 import time
 from functional_tests import FunctionalTest, ROOT, USERS
 from ddt import ddt, data, unpack
-import functional_tests
 from selenium.webdriver.support.ui import WebDriverWait
+
 
 @ddt
 class AnswerQuestion (FunctionalTest):
 
     def setUp(self):  
         self.url = ROOT + '/default/user/login'        
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
 
     #this test below is ok on basis there is only ONE group to join
     @data((USERS['USER6'], USERS['PASSWORD6']),
@@ -34,16 +34,16 @@ class AnswerQuestion (FunctionalTest):
 
         self.url = ROOT + "/accessgroups"
 
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
         time.sleep(1)
 
         self.browser.find_element_by_xpath("//input[@value='Join Group']").click()
 
         time.sleep(3)
-        #target = self.browser.find_element_by_css_selector("#target")
-        #self.assertIn('Challenge accepted', target.text)
-        body = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_tag_name('body'))
+        # target = self.browser.find_element_by_css_selector("#target")
+        # self.assertIn('Challenge accepted', target.text)
+        body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
         self.assertIn('You joined the group', body.text)
 
         self.url = ROOT + '/default/user/logout'
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
--- fts/test_ph4_6viewquest.py
+++ fts/test_ph4_6viewquest.py
@@ -2,22 +2,22 @@
 # if registration is successful this may work but lets
 # try and get user logged in first
 
-import time
 from functional_tests import FunctionalTest, ROOT, USERS
 from ddt import ddt, data, unpack
 import functional_tests
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 @ddt
 class AnswerQuestion (FunctionalTest):
 
     def setUp(self):  
         self.url = ROOT + '/default/user/login'
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
 
-    @data((USERS['USER2'], USERS['PASSWORD2'],'Users have resolved'),
-          (USERS['USER5'], USERS['PASSWORD5'],'Question not available for viewing'))
+    @data((USERS['USER2'], USERS['PASSWORD2'], 'Users have resolved'),
+          (USERS['USER5'], USERS['PASSWORD5'], 'Question not available for viewing'))
     @unpack
     def test_challenge(self, user, passwd, result):
         mailstring = user + '@user.com'
@@ -32,15 +32,15 @@ class AnswerQuestion (FunctionalTest):
         submit_button.click()
         time.sleep(1)
 
-        #self.url = ROOT + "/viewquest/index/4"
+        # self.url = ROOT + "/viewquest/index/4"
         self.url = functional_tests.questref
 
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
         time.sleep(2)
 
-        body = WebDriverWait(self, 10).until(lambda self : self.browser.find_element_by_tag_name('body'))
+        body = WebDriverWait(self, 10).until(lambda self: self.browser.find_element_by_tag_name('body'))
         self.assertIn(result, body.text)
 
         self.url = ROOT + '/default/user/logout'
-        get_browser=self.browser.get(self.url)
+        get_browser = self.browser.get(self.url)
         time.sleep(1)
